75e68dfd8aebd68075129a3976406c852ff43abd
rajab Sakalaki <rjbsklk@gmail.com>
2023-12-06 10:25:54 +0200

    Creating DTOs

---
+++ Core/Dtos/UpdatPermissionDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace identityEx.Core.Dtos
+{
+    public class UpdatPermissionDTO
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+    }
+}
